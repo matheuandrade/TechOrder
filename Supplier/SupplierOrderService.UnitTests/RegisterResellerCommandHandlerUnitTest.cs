5c525423883b544cc2f954b7e291400139ff5211
Matheus Andrade <matheus.andrade@ntconsultcorp.com>
2025-03-13 13:12:15 -0300

    adding ShouldRegisterCompanySuccessfully unit test

--- Supplier/SupplierOrderService.UnitTests/RegisterResellerCommandHandlerUnitTest.cs
+++ Supplier/SupplierOrderService.UnitTests/RegisterResellerCommandHandlerUnitTest.cs
@@ -36,4 +36,30 @@ public class RegisterResellerCommandHandlerUnitTest
         Assert.NotNull(response.Error);
         Assert.Equal("Empresa já cadastrada", response.Error.Description);
     }
+
+    [Fact]
+    public async Task ShouldRegisterCompanySuccessfully()
+    {
+        var cnpj = "cpnj";
+
+        var command = new RegisterResellerCommand(new Application.Dtos.ResellerDto(cnpj));
+
+        _mockRepository
+        .Setup(x => x.AnyAsync(It.IsAny<System.Linq.Expressions.Expression<Func<Core.Entities.Reseller, bool>>>(), It.IsAny<CancellationToken>()))
+        .ReturnsAsync(false);
+
+        var response = await _handler.Handle(command, new CancellationToken());
+
+        Assert.True(response.IsSuccess);
+        Assert.IsType<SharedKernel.Result<Guid>>(response);
+
+        _mockLogger.Verify(
+            x => x.Log(
+                LogLevel.Information,  // Specify the log level
+                It.IsAny<EventId>(),    // EventId is typically not important in this case
+                It.Is<It.IsAnyType>((v, t) => v.ToString().Contains($"New reseller created successfully with ID: {response.Value} and CNPJ: {cnpj}")),
+                It.IsAny<Exception>(),  // Exception, if any, can be set to any
+                It.IsAny<Func<It.IsAnyType, Exception, string>>()),  // Format delegate
+            Times.Once);  // Verify it was called exactly once
+    }
 }
