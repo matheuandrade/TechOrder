255cd7bcc9919a19cc4fd5019ad1f8635659f6c0
Matheus Andrade <matheus.andrade@ntconsultcorp.com>
2025-03-13 12:54:47 -0300

    adding ShouldReturnErrorMessageForACompanyWhoAlreadyExists unite test

--- Supplier/SupplierOrderService.Application/Reseller/Register/RegisterResellerCommandHandler.cs
+++ Supplier/SupplierOrderService.Application/Reseller/Register/RegisterResellerCommandHandler.cs
@@ -1,13 +1,12 @@
 ﻿using MediatR;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using SharedKernel;
-using SupplierOrderService.Application.Abstractions.Data;
 using SupplierOrderService.Core.Entities.Errors;
+using SupplierOrderService.Core.Interfaces;
 
 namespace SupplierOrderService.Application.Reseller.Register;
 
-public class RegisterResellerCommandHandler(IApplicationDbContext context,
+public class RegisterResellerCommandHandler(IRepository<Core.Entities.Reseller> repository,
                                             ILogger<RegisterResellerCommandHandler> logger)
     : IRequestHandler<RegisterResellerCommand, Result<Guid>>
 {
@@ -17,7 +16,7 @@ public class RegisterResellerCommandHandler(IApplicationDbContext context,
         logger.LogInformation("Attempting to register reseller with CNPJ: {CNPJ}", request.Reseller.CNPJ);
 
         // Verificando se já existe um revendedor com o mesmo CNPJ
-        if (await context.Resellers.AnyAsync(x => x.CNPJ == request.Reseller.CNPJ, cancellationToken))
+        if (await repository.AnyAsync(x => x.CNPJ == request.Reseller.CNPJ, cancellationToken))
         {
             // Logando uma tentativa de registro com falha devido ao CNPJ já existente
             logger.LogWarning("Registration failed: Reseller with CNPJ {CNPJ} already exists", request.Reseller.CNPJ);
@@ -37,8 +36,8 @@ public class RegisterResellerCommandHandler(IApplicationDbContext context,
         logger.LogInformation("Persisting new reseller with CNPJ: {CNPJ}", request.Reseller.CNPJ);
 
         // Adicionando o novo revendedor ao contexto e salvando no banco de dados
-        await context.Resellers.AddAsync(newReseller, cancellationToken);
-        await context.SaveChangesAsync(cancellationToken);
+        await repository.AddAsync(newReseller, cancellationToken);
+        await repository.SaveChangesAsync(cancellationToken);
 
         // Logando o sucesso da criação do revendedor com o ID gerado
         logger.LogInformation("New reseller created successfully with ID: {ResellerId} and CNPJ: {CNPJ}", newReseller.Id, request.Reseller.CNPJ);
--- Supplier/SupplierOrderService.UnitTests/RegisterResellerCommandHandlerUnitTest.cs
+++ Supplier/SupplierOrderService.UnitTests/RegisterResellerCommandHandlerUnitTest.cs
@@ -1,9 +1,39 @@
-ï»¿namespace SupplierOrderService.UnitTests;
+using Microsoft.Extensions.Logging;
+using Moq;
+using SupplierOrderService.Application.Reseller.Register;
+using SupplierOrderService.Core.Interfaces;
+
+namespace SupplierOrderService.UnitTests;
 
 public class RegisterResellerCommandHandlerUnitTest
 {
+    private readonly Mock<IRepository<Core.Entities.Reseller>> _mockRepository;
+    private readonly Mock<ILogger<RegisterResellerCommandHandler>> _mockLogger;
+    private readonly RegisterResellerCommandHandler _handler;
+
     public RegisterResellerCommandHandlerUnitTest()
     {
+        _mockRepository = new();
+        _mockLogger = new();
 
+        _handler = new RegisterResellerCommandHandler(_mockRepository.Object, _mockLogger.Object);
+    }
+
+    [Fact]
+    public async Task ShouldReturnErrorMessageForACompanyWhoAlreadyExists()
+    {
+        var cnpj = "cpnj";
+
+        var command = new RegisterResellerCommand(new Application.Dtos.ResellerDto(cnpj));
+
+        _mockRepository
+        .Setup(x => x.AnyAsync(It.IsAny<System.Linq.Expressions.Expression<Func<Core.Entities.Reseller, bool>>>(), It.IsAny<CancellationToken>()))
+        .ReturnsAsync(true);
+
+        var response = await _handler.Handle(command, new CancellationToken());
+
+        Assert.False(response.IsSuccess);
+        Assert.NotNull(response.Error);
+        Assert.Equal("Empresa já cadastrada", response.Error.Description);
     }
 }
